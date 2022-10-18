2e913d299ce810d532f61893c3097bdc3d98f15d
Peter <905609587@qq.com>
2022-10-18 15:45:50 +0800

    think nega num of filk

--- lab4/flik/FilkTest.java
+++ lab4/flik/FilkTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.*;
 public class FilkTest {
     @Test
     public void test0() {
-        int i = 0;
-        int j = 0;
-        for( ; i < 10000; i++, j++) {
+        int i = -1000;
+        int j = -1000;
+        for ( ; i < 10000; i++, j++) {
             assertTrue(Flik.isSameNumber(i, j));
         }
     }
