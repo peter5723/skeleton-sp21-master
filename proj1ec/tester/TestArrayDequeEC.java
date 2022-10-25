a90660f6e4e7480f508ee9f8c21bd2255be1e489
peter <905609587@qq.com>
2022-10-25 19:39:03 +0800

    projet 1 ec 2try

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -7,7 +7,7 @@ public class TestArrayDequeEC {
     @Test
     public void randomTest1() {
         int N = 50000;
-
+        StringBuilder s = new StringBuilder();
         ArrayDequeSolution<Integer> arr1 = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> arr2 = new StudentArrayDeque<>();
         for (int i = 0; i <= N; i++) {
@@ -16,19 +16,24 @@ public class TestArrayDequeEC {
             if (randomNum == 0) {
                 arr1.addLast(testnum);
                 arr2.addLast(testnum);
+                s.append("addLast("+testnum+")\n");
             } else if (randomNum == 1) {
                 arr1.addFirst(testnum);
                 arr2.addFirst(testnum);
+                s.append("addFirst("+testnum+")\n");
             } else if (randomNum == 2) {
-                assertEquals("oh, fuck, your size"+arr2.size()+"not equal to" +
-                        "correct size" + arr1.size() ,arr1.size(), arr2.size());
+                s.append("size()\n");
+                assertEquals(s.toString() ,arr1.size(), arr2.size());
             } else if (randomNum == 3 && arr1.size() > 0) {
-                assertEquals("o, your remove first num is not equal to correct num: ",arr1.removeFirst(), arr2.removeFirst());
+                s.append("removeFirst()\n");
+                assertEquals(s.toString(),arr1.removeFirst(), arr2.removeFirst());
             } else if (randomNum == 4 && arr1.size() > 0) {
-                assertEquals("o, your remove last num is not equal to correct num: ",arr1.removeLast(), arr2.removeLast());
+                s.append("removeLast()\n");
+                assertEquals(s.toString(),arr1.removeLast(), arr2.removeLast());
             } else if (randomNum == 5 && arr1.size() > 0) {
                 int temp = StdRandom.uniform(0, arr2.size());
-                assertEquals("o, your num from get is not equal to correct num: ",arr1.get(temp), arr2.get(temp));
+                s.append("get("+temp+")\n");
+                assertEquals(s.toString(),arr1.get(temp), arr2.get(temp));
             }
         }
     }
