1bb08d25ad92e7a016635bfa959633b7da890fb4
peter <905609587@qq.com>
2023-02-25 16:19:40 +0800

    hhh

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -13,7 +13,7 @@ public class Main {
      * Usage: java gitlet.Main ARGS, where ARGS contains
      * <COMMAND> <OPERAND1> <OPERAND2> ...
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //if args is empty
         if (args.length == 0) {
             System.out.println("Please enter a command.");
@@ -22,7 +22,11 @@ public class Main {
         String firstArg = args[0];
         switch (firstArg) {
             case "init":
-                Repository.initGitlet();
+                try {
+                    Repository.initGitlet();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
                 break;
             case "add":
                 Repository.judgeInit();
